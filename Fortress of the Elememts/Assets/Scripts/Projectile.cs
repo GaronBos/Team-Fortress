ef0574bef84ca1144601cff2a9f531d3148d198c
Damocles <104439141+ShotgunOdy@users.noreply.github.com>
2022-10-03 16:05:18 +0200

    boom

---
+++ Fortress of the Elememts/Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+   
+        private Vector3 FiringPoint;
+
+        [SerializeField]
+        private float ProjectileSpeed;
+
+        [SerializeField]
+        private float maxProjectileDistance;
+
+        void Start()
+        {
+            FiringPoint = transform.position;
+        }
+
+        void Update()
+        {
+            MoveProjectile();
+        }
+
+        void MoveProjectile()
+        {
+            if (Vector3.Distance(FiringPoint, transform.position) > maxProjectileDistance)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * ProjectileSpeed * Time.deltaTime);
+            }
+        }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Health>().currentHealth -= 40;
+            Destroy(this.gameObject);
+        }
+
+
+    }
+}
